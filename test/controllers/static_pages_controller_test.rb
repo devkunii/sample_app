7b5250c00f16d850d9d271d5d4f46490eabe763c
MacUser <sou.8216028k@ozzio.jp>
2018-02-05 21:16:45 +0900

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -10,5 +11,21 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_help_url
     assert_response :success
   end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+  end
+
+  test "should get root" do
+   get FILL_IN
+   assert_response FILL_IN
+  end
+
 
 end
